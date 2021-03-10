81a1a7e74ef7f1065d82892f2257f17ea665bd67
FaustTobias <fausttobias@users.noreply.github.com>
2021-03-10 22:00:17 +0100

    Add check flag to prettier

--- scripts/add-prettier-format.js
+++ scripts/add-prettier-format.js
@@ -4,6 +4,6 @@ const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 pkg.scripts = pkg.scripts ?? {};
 pkg.scripts.format = "prettier --write . **/*.svelte";
-pkg.scripts["format:check"] = "prettier . **/*.svelte";
+pkg.scripts["format:check"] = "prettier --check . **/*.svelte";
 
 fs.writeFileSync("package.json", JSON.stringify(pkg));
