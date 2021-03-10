4d73e2c82bc8db693b05896f1de556a7a411f73f
FaustTobias <fausttobias@users.noreply.github.com>
2021-03-10 19:02:33 +0100

    Fix snowpack plugins

--- scripts/add-snowpack-plugins.js
+++ scripts/add-snowpack-plugins.js
@@ -4,8 +4,8 @@ let snowpack = fs.readFileSync("snowpack.config.js", "utf8");
 const plugins = process.argv.slice(2);
 
 snowpack = snowpack.replace(
-  /plugins:\s*\[/,
-  plugins.map(JSON.stringify).join(", ")
+  /(plugins:\s*\[)/,
+  "$1" + plugins.map(JSON.stringify).join(", ")
 );
 
 fs.writeFileSync("snowpack.config.js", snowpack);
