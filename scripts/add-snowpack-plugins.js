5fd2e3df94887ed00113e75cfca319efec925c05
Tobias Faust <tf@ambimax.de>
2021-03-18 20:17:41 +0100

    Add snowpack-tailwind template (WIP)

--- scripts/add-snowpack-plugins.js
+++ scripts/add-snowpack-plugins.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 
-let snowpack = fs.readFileSync("snowpack.config.js", "utf8");
-const plugins = process.argv.slice(2);
+module.exports = (plugins) => {
+    let snowpack = fs.readFileSync("snowpack.config.js", "utf8");
 
-snowpack = snowpack.replace(/(plugins:\s*\[)/, "$1" + plugins.map(JSON.stringify).join(", ") + ",");
+    snowpack = snowpack.replace(/(plugins:\s*\[)/, "$1" + plugins.map(JSON.stringify).join(", ") + ",");
 
-fs.writeFileSync("snowpack.config.js", snowpack);
+    fs.writeFileSync("snowpack.config.js", snowpack);
+};
+
+if (require.main === module) {
+    module.exports(process.argv.slice(2));
+}
