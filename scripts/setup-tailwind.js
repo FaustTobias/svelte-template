6a2f26d52f031fe2d2c4fcc453a9521afdddc76a
Tobias Faust <tf@ambimax.de>
2021-03-18 21:12:10 +0100

    Improve tailwind config

--- scripts/setup-tailwind.js
+++ scripts/setup-tailwind.js
@@ -9,26 +9,23 @@ const isSnowpack = fs.existsSync("snowpack.config.js");
 
 fs.writeFileSync(
     "tailwind.config.js",
-    `const production = ${
-        isRollup ? "!process.env.ROLLUP_WATCH" : 'NODE_ENV === "production"'
-    }; // or some other env var like NODE_ENV
-module.exports = {
-  future: { // for tailwind 2.0 compat
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
-  },
-  plugins: [
-    // for tailwind UI users only
-    // require('@tailwindcss/ui'),
-    // other plugins here
-  ],
-  purge: {
-    content: [
-      "./src/**/*.svelte",
-      // may also want to include base index.html
+    `const production = ${isRollup ? "!process.env.ROLLUP_WATCH" : 'process.env.NODE_ENV === "production"'};
+
+    module.exports = {
+    future: {
+        purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+    },
+    plugins: [
+        // for tailwind UI users only
+        // require('@tailwindcss/ui'),
     ],
-    enabled: production // disable purge in dev
-  },
+    purge: {
+        content: [
+        "./src/**/*.svelte",
+        ],
+        enabled: production // disable purge in dev
+    },
 };
 `
 );
